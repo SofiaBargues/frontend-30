265700b8972d16228fca0a4452d7ee2e4ab29869
Sofia Bargues <barguessofia@gmail.com>
2023-06-24 14:05:03 +0100

    new css banner

--- app/page.tsx
+++ app/page.tsx
@@ -27,9 +27,7 @@ export default function Home() {
       <main className="flex flex-col items-center  w-full">
         <div className="flex flex-col items-center w-full w-min-3xl">
           <section className="flex flex-col items-center w-full">
-            <header className="bg-secondary w-full p-4">
-              <h1 className="text-center text-2xl">HTML</h1>
-            </header>
+            <SectionHeader text="HTML" />
             <div className="flex flex-col items-center">
               <Step text="newEnglish" type="start" alignment="left-0" />
               <Step text="Bootcamp" type="course" alignment="left-11" />
@@ -40,6 +38,13 @@ export default function Home() {
               <Step text="Get a Job" type="course" alignment="right-16" />
             </div>
           </section>
+          <section className="flex flex-col items-center w-full">
+            <SectionHeader text="CSS" />
+            <div className="flex flex-col items-center">
+              <Step text="newEnglish" type="start" alignment="left-0" />
+              <Step text="Bootcamp" type="course" alignment="left-11" />
+            </div>
+          </section>
         </div>
       </main>
       <footer className="footer footer-center p-4 bg-base-300 text-base-content mt-20 fixed bottom-0">
@@ -48,6 +53,14 @@ export default function Home() {
         </div>
       </footer>
     </div>
+  );
+}
+
+function SectionHeader({ text }: { text: string }) {
+  return (
+    <header className="bg-secondary w-full p-4">
+      <h1 className="text-center text-2xl">{text}</h1>
+    </header>
   );
 }
 
