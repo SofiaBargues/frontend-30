48e3a38709731323b672350ed32872a660db6c57
Sofia Bargues <barguessofia@gmail.com>
2023-08-15 13:58:17 +0100

    barra de theme

--- app/page.tsx
+++ app/page.tsx
@@ -250,7 +250,7 @@ const JSItems: Item[] = [
     name: "Basic JavaScript",
     description: "Empty  2",
     type: "course",
-    completed: false,
+    completed: true,
   },
 ];
 const ReactItems: Item[] = [
@@ -399,7 +399,7 @@ export default function Home() {
 function ThemeChange({
   dropdownClasses = "",
   btnClasses = "btn-ghost",
-  contentClasses = "mt-16",
+  contentClasses = "mt-16 border-2 border-primary",
 }) {
   useEffect(() => {
     themeChange(false);
@@ -441,44 +441,46 @@ function ThemeChange({
       </div>
       <div
         tabIndex={0}
-        className={`dropdown-content z-[1] menu  bg-base-200 text-base-content rounded-box top-px h-[70vh] max-h-96 w-56 overflow-y-auto shadow ${contentClasses}`}
+        className={`dropdown-content z-[1] menu bg-base-200 text-base-content rounded-box top-px  p-3 shadow ${contentClasses}`}
       >
-        <div className="grid grid-cols-1 gap-3 p-3">
-          {themes.map((theme) => (
-            <button
-              key={theme.id}
-              className="outline-base-content overflow-hidden rounded-lg text-left"
-              data-set-theme={theme.id}
-              data-act-class="[&_svg]:visible"
-            >
-              <div
-                data-theme={theme.id}
-                className="bg-base-100 text-base-content w-full cursor-pointer font-sans"
+        <div className="overflow-y-auto h-[70vh] max-h-96 w-56">
+          <div className="grid grid-cols-1 gap-3 pr-2">
+            {themes.map((theme) => (
+              <button
+                key={theme.id}
+                className="outline-base-content overflow-hidden rounded-lg text-left"
+                data-set-theme={theme.id}
+                data-act-class="[&_svg]:visible"
               >
-                <div className="grid grid-cols-5 grid-rows-3">
-                  <div className="col-span-5 row-span-3 row-start-1 flex items-center gap-2 px-4 py-3">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      className="invisible h-3 w-3 shrink-0"
-                    >
-                      <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z" />
-                    </svg>
-                    <div className="flex-grow text-sm">{theme.id}</div>
-                    <div className="flex h-full flex-shrink-0 flex-wrap gap-1">
-                      <div className="bg-primary w-2 rounded" />
-                      <div className="bg-secondary w-2 rounded" />
-                      <div className="bg-accent w-2 rounded" />
-                      <div className="bg-neutral w-2 rounded" />
+                <div
+                  data-theme={theme.id}
+                  className="bg-base-100 text-base-content w-full cursor-pointer font-sans"
+                >
+                  <div className="grid grid-cols-5 grid-rows-3">
+                    <div className="col-span-5 row-span-3 row-start-1 flex items-center gap-2 px-4 py-3">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="invisible h-3 w-3 shrink-0"
+                      >
+                        <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z" />
+                      </svg>
+                      <div className="flex-grow text-sm">{theme.id}</div>
+                      <div className="flex h-full flex-shrink-0 flex-wrap gap-1">
+                        <div className="bg-primary w-2 rounded" />
+                        <div className="bg-secondary w-2 rounded" />
+                        <div className="bg-accent w-2 rounded" />
+                        <div className="bg-neutral w-2 rounded" />
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </button>
-          ))}
+              </button>
+            ))}
+          </div>
         </div>
       </div>
     </div>
@@ -583,6 +585,15 @@ function Hero({}: {}) {
   );
 }
 
+interface StepProps {
+  alignment: string;
+  type: string;
+  text: string;
+  description: string;
+  link?: string;
+  disabled?: boolean;
+}
+
 function Step({
   alignment,
   type,
@@ -590,14 +601,7 @@ function Step({
   description,
   link,
   disabled = false,
-}: {
-  alignment: string;
-  type: string;
-  text: string;
-  description: string;
-  link?: string;
-  disabled?: boolean;
-}) {
+}: StepProps) {
   let icon = <></>;
 
   if (type == "course") {
