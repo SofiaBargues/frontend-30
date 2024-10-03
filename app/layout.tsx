9508edcf4d2f5d7b35dccc33cb1f0176c26cc4d4
Sofia Bargues <barguessofia@gmail.com>
2024-10-03 15:28:26 +0100

    metadatabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Frontend 30",
   description: "Learn or test your frontend skills with these challenges",
+  metadataBase: process.env.VERCEL_URL
+    ? new URL(`https://frontend-30.vercel.app/`)
+    : new URL(`http://localhost:${process.env.PORT || 3000}`),
 };
 
 export default function RootLayout({
