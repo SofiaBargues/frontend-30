cdb455e081157913856c725f8a22c184dc862bec
Sofia Bargues <barguessofia@gmail.com>
2024-10-26 11:17:20 +0100

    fds

--- app/component/StarOnGithubButton.tsx
+++ app/component/StarOnGithubButton.tsx
@@ -8,7 +8,7 @@ export function StarOnGithubButton() {
       href="https://github.com/SofiaBargues/frontend-30"
       target="_blank"
       rel="noopener noreferrer"
-      className="group flex items-center gap-1.5 rounded-lg border bg-gray-100 bg-gradient-to-r p-3 px-4 py-2 shadow-md shadow-primary transition-shadow hover:shadow-lg hover:shadow-primary-focus"
+      className="group flex items-center gap-1.5 rounded-lg border bg-gray-100 bg-gradient-to-r p-3 px-4 py-2 shadow-md transition-shadow hover:shadow-md hover:shadow-primary-focus"
     >
       <Github className="h-4 w-4 text-primary-content" />
       <span className="hidden text-sm font-semibold text-primary-content sm:block">
